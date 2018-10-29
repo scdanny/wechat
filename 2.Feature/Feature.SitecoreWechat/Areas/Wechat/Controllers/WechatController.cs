52a1f512e29d2ea8c781d3edcc6b2f516004a23f
Fangzhou SHi <fangzhou.shi@live.com>
2018-10-29 17:21:49 +0800

    添加项目文件。

---
+++ 2.Feature/Feature.SitecoreWechat/Areas/Wechat/Controllers/WechatController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feature.SitecoreWechat.Areas.Wechat.Controllers
+{
+    public class WechatController : Controller
+    {
+        // GET: Wechat/Wechat
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string VerifySignature()
+        {
+            if(Request["echostr"].ToString() !=null)
+            {
+                string signature = Request["signature"];
+                string timestamp = Request["timestamp"];
+                string nonce = Request["nonce"];
+                string echostr = Request["echostr"];
+                
+                Sitecore.Diagnostics.Log.Info("Signature:" + signature + " " + "Timestamp:" + timestamp + " " + "Nonce:" + nonce + " " + "Echostr:" + echostr, this);
+
+                return echostr;
+
+            }else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Feature.SitecoreWechat.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Feature.SitecoreWechat.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
